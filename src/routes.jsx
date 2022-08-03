c238d8f562829e36c5bedf2aef16c465d4473c09
mendia <suze0526@gmail.com>
2022-08-03 22:50:47 +0800

    feat: 增加约定式路由功能

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,19 @@
 import Layout from './layout';
-import Home from './pages/home';
-import NoMatch from './pages/noMatch';
-import QueryTable from './pages/queryTable';
+import Login from './pages/notBelongLayout/login';
+import NoMatch from './pages/notBelongLayout/noMatch';
+
+// 使用约定式路由,pages 下的文件夹名称为 path,自动注册.notBelongLayout 例外,比如有些 login 不应该在 layout 下,比如 noMatch 的 path 应该是*,不应该是 noMatch
+const pageObj = import.meta.glob('./pages/*/index.jsx', { eager: true });
+const pageList = Object.entries(pageObj).map(([key, mode]) => ({
+  path: `/${key.split('/')[2]}`,
+  element: <mode.default />,
+}));
 
 export default [
+  { path: '/login', element: <Login /> },
   {
     path: '/',
     element: <Layout />,
-    children: [
-      { path: '/home', element: <Home /> },
-      { path: '/queryTable', element: <QueryTable /> },
-      { path: '*', element: <NoMatch /> },
-    ],
+    children: [...pageList, { path: '*', element: <NoMatch /> }],
   },
 ];
