62b9214dd6eabfcfa7f67250d758b12ab5206820
mendia <suze0526@gmail.com>
2022-08-06 16:25:28 +0800

    chore: 切换到 less

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import '@icon-park/react/styles/index.css';
 import { ConfigProvider } from 'antd';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.less';
 import zhCN from 'antd/es/locale/zh_CN';
 import moment from 'moment';
 import 'moment/dist/locale/zh-cn';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
+import './index.less';
 import App from './pages/index';
 
 moment.locale('zh-cn');
